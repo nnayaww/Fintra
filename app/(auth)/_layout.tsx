cb6d9f7338cc44f5e92c7b6f74ad796f0bb42104
Nana Yaw <nnayaww@gmail.com>
2025-07-27 16:12:49 +0000

    layout fixes

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,19 +2,36 @@ import { SignUpProvider } from "@/context/SignUpContext";
 import { useTheme } from "@/lib/ThemeContext";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 const AuthLayout = () => {
   const { theme } = useTheme();
+
   return (
     <SignUpProvider>
-      <StatusBar
-        style={theme === "dark" ? "light" : "dark"}
-        backgroundColor={theme === "dark" ? "#181A20" : "#ffffff"}
-        translucent={true}
-      />
-      <Stack screenOptions={{ headerShown: false }} />
+      <SafeAreaView
+        style={[
+          styles.container,
+          { backgroundColor: theme === "dark" ? "#181A20" : "#ffffff" },
+        ]}
+        edges={["top", "left", "right"]}
+      >
+        <StatusBar
+          style={theme === "dark" ? "light" : "dark"}
+          backgroundColor={theme === "dark" ? "#181A20" : "#ffffff"}
+          translucent={true}
+        />
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
     </SignUpProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default AuthLayout;
